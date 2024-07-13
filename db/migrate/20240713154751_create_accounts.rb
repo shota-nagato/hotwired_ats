90411c37181f892da78d60755f55e3f22ddb2d40
shota-nagato <shota.nagato@gmail.com>
2024-07-14 01:03:19 +0900

    Userモデル作成

--- db/migrate/20240713154751_create_accounts.rb
+++ db/migrate/20240713154751_create_accounts.rb
@@ -1,6 +1,6 @@
 class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
-    create_table :accounts do |t|
+    create_table :accounts, id: :uuid do |t|
       t.string :name, null: false
 
       t.timestamps
