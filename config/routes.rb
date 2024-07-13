bbd1cc3ffdf9000331da8ae5f0595dbce666c4fe
shota-nagato <shota.nagato@gmail.com>
2024-07-14 01:07:30 +0900

    ログイン前後のルートパスを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "dashboard#show"
+
+  authenticated :user do
+    root to: "dashboard#show", as: :user_root
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
