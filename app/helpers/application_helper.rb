bbe759d5a0fd87b12cfd6bfa2a514d8e955e274e
shota-nagato <shota.nagato@gmail.com>
2024-07-14 11:13:05 +0900

    standardrb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def flash_class(level)
     case level.to_sym
     when :notice
-      'bg-blue-900 border-blue-900'
+      "bg-blue-900 border-blue-900"
     when :success
-      'bg-green-900 border-green-900'
+      "bg-green-900 border-green-900"
     when :alert
-      'bg-red-900 border-red-900'
+      "bg-red-900 border-red-900"
     when :error
-      'bg-red-900 border-red-900'
+      "bg-red-900 border-red-900"
     else
-      'bg-blue-900 border-blue-900'
+      "bg-blue-900 border-blue-900"
     end
   end
 end
