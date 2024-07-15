588998e613afdcea5a7730072437f4d9e5cacf93
shota-nagato <shota.nagato@gmail.com>
2024-07-15 17:24:23 +0900

    test追加　

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :job do
-    title { "MyString" }
-    status { "MyString" }
-    job_type { "MyString" }
-    location { "MyString" }
-    account { nil }
+    title { "job01" }
+    status { "Open" }
+    job_type { "Full time" }
+    location { "Tokyo" }
+    association :account
   end
 end
