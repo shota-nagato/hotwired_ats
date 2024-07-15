2a2a762acb2fb9698ee7a8996c1d88e88de2cc2c
shota-nagato <shota.nagato@gmail.com>
2024-07-15 16:53:14 +0900

    jobのCRUD

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,19 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
 import consumer from "./channels/consumer"
 import CableReady from "cable_ready"
 import mrujs from "mrujs"
 import { CableCar } from "mrujs/plugins"
 
-import "trix"
-import "@rails/actiontext"
-
 mrujs.start({
   plugins: [
-    new CableCar(CableReady)
+    new CableCar(CableReady, { mimeType: "application/vnd.cable-ready.json" })
   ]
 })
+
+CableReady.initialize({ consumer })
+
+import "trix"
+import "@rails/actiontext"
